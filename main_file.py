d492923487f6ecb4736d9d2aba8dc0b73a3b24ed
Ansh <anshmakkar64@gmail.com>
2024-03-03 23:53:58 +0530

    created contact page outline.

--- main_file.py
+++ main_file.py
@@ -15,7 +15,7 @@ st.set_page_config(
 selected=option_menu(
 
     menu_title=None,
-    options=["Home","Experience","Projects","Contact_Me"],
+    options=["Home","Experience","Projects","Contact Me"],
     icons=["house","clipboard-data-fill","book","envelope"],
     menu_icon="cast",
     default_index=0,
@@ -23,3 +23,47 @@ selected=option_menu(
 )
 if selected:
     st.write(f"{selected}")
+
+if selected=="Contact Me":
+    st.markdown("<h1 style='text-align: center;'>CONTACT ME</h1>", unsafe_allow_html=True)
+    st.write(" ")
+    st.write(" ")
+    col1, col2 = st.columns(2)
+    with col1:
+        first_name=st.text_input("First Name :")
+        
+        mobile_number=st.text_input("Contact Number :")
+        
+    with col2:
+        last_name=st.text_input("Last Name :")
+        email_address=st.text_input("Email Address :")
+        
+    st.text_area("You Can Leave Your Messeage Here .....")
+    st.write("<div style='text-align: center;'>", unsafe_allow_html=True)
+    col11, col12, col13 , col14, col15 = st.columns(5)
+
+    with col11:
+        pass
+    with col12:
+        pass
+    with col14:
+        pass
+    with col15:
+        pass
+    with col13 :
+        form_submit_button = st.button('Button')
+    # form_submit_button=st.button("Sumbit")
+    errors=False
+    if first_name and len(first_name)<3 or form_submit_button and len(first_name)<3:
+        st.error("Please Enter A Valid Name")  
+        errors=True      
+        
+    if mobile_number and len(mobile_number)!=10 or form_submit_button and len(mobile_number)!=10:
+        st.error("please Enter Valid Mobile Number.")
+        errors=True
+    if email_address and ("@" not in email_address or "com" not in email_address) or form_submit_button and ("@" not in email_address or "com" not in email_address):
+            st.error("Please Enter a Valid Email Id")
+            errors=True
+    
+    if form_submit_button and not errors:        
+        st.success(f"Thankyou! {first_name} , I will Contact as Soon as Possible.")
