26c6e78a5e1a07d052bb00c5c9241781ee6afa86
Ansh <anshmakkar64@gmail.com>
2024-03-10 22:40:47 +0530

    added socail media links on home page

--- home_page.py
+++ home_page.py
@@ -2,11 +2,15 @@ import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 EMAIL="anshmakkar64@gmail.com"
+Socail_Media_Links={"Linkedin":"https://www.linkedin.com/in/ansh-makkar/",
+                    "GitHub":"https://github.com/ansh-makkar",
+                    "Kaggle":"https://www.kaggle.com/anshmakkar"
+                    }
 
 def home_page_function():
     col1,col2=st.columns(2)
     with col1:
-        st.image("profile-pic.png",width=350)
+        st.image("profile-pic.png",width=280)
     with col2:
         st.title("Ansh Makkar")
         st.write("Data Analyst at Quant Jugglers")
@@ -14,3 +18,7 @@ def home_page_function():
         st.write("",EMAIL)
     st.write("#")
     st.write("#")
+    cols=st.columns(len(Socail_Media_Links))
+    for index,(platform,links) in enumerate(Socail_Media_Links.items()):
+        cols[index].write(f"[{platform}]({links})")
+    
