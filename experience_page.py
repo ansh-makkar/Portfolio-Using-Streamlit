3232c08af7217cfb04c422b4c93ab3309c330c0d
Ansh <anshmakkar64@gmail.com>
2024-03-13 21:35:34 +0530

    upadated experience page

--- experience_page.py
+++ experience_page.py
@@ -3,20 +3,34 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 def experience_function():
+
     st.write("#")
+    st.write("---")
     col1,col2,col3=st.columns(3)
     with col1:
         st.subheader("Data Analyst- Quant Jugglers")
         # st.subheader("Quant Jugglers")
     with col3:
         st.subheader("May,2023-Present")
-    st.write("""
-    
-    -Backtest Indices(Banknifty, Nifty, Finnifty etc.) from Last 5-7 years.
-    -Create Strategy For Algo-Tradign Using Python.
-    -Create Dashboard using Libraries like Streamlit, Dash, flask etc. To Display Current P&L.
-    -Clean Backadjusted Datas and Extract Only Useful.
+    st.write("#")
+    st.write("""    
+        - Clean Backadjusted Datas and Extract Only Useful Insights
+        - Backtest Indices(Banknifty, Nifty, Finnifty etc.) from Last 5-7 years.
+        - Create Strategy For Algo-Tradign Using Python.
+        - Create Dashboard using Libraries like Streamlit, Dash, flask etc. To Display Current P&L.        
+        - Run Algo-Codes on AWS Platform using Services Like EC2.
+    """)
 
-    
-    
+    st.write("#")
+    st.write("---")
+    col1,col2,col3=st.columns(3)
+    with col1:
+        st.subheader("Automation Engineer- Sysmat Reasearch Solutions LLP")
+    with col3:
+        st.subheader("Nov,2022-April,2023")
+    st.write("#")
+    st.write("""    
+        - Automated Websites/Windows Applications/Android Applications by Using Libraries like ADB,Selenium etc. 
+        - Developed in House Project To Automate Download of Android Applications From Play Store Multiple Times.
+        - Maintained Data & Analyzed it to show in form of Dashboards.
     """)
